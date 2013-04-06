924a8e6713bd4e5492cee2d889e4739ba6a4d325
William espindola <williamespindola@instinfo.com.br>
2013-04-06 16:37:25 -0300

    sample config

--- config.sample.php
+++
@@ -1,5 +0,0 @@
-<?php
-// esse arquivo tem que ser salvo na raiz do projeto como config.php
-
-define('CONFIG_S3_KEY', 'coloque aqui sua chave');
-define('CONFIG_S3_SECRET', 'coloque aqui seu segredo');
