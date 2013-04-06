6cd7d284de1700f5db70a1fe6593fcd264444303
William espindola <williamespindola@instinfo.com.br>
2013-04-06 16:34:28 -0300

    upload simple file

--- web/index.php
+++ web/index.php
@@ -4,6 +4,7 @@ require '../bootstrap.php';
 
 use Aws\S3\S3Client;
 use Aws\Common\Enum\Region;
+use Aws\S3\Model;
 
 // Instantiate the S3 client with your AWS credentials and desired AWS region
 $client = S3Client::factory(array(
@@ -19,8 +20,15 @@ $result = $client->createBucket(
     )
 );
 
-var_dump($result);
-
 // Wait until the bucket is created
 $client->waitUntil('BucketExists', array('Bucket' => $bucket));
 
+//upload simple file
+$client->putObject(array(
+    'Bucket'     => 'aws-hackathon-phpsp-bucket-teste',
+    'Key'        => 'index.html',
+    'SourceFile' => 'index.html',
+    'Metadata'   => array(
+        'Content-Type' => 'text/html; charset=utf-8'
+    )
+));
